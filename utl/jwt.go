8f6ec36459ab39c85f44eca750199dd3c3612f1e
Duy Nguyen <ndhduy.7798@gmail.com>
2020-01-09 09:46:58 +0700

    Update JWT parsed with claims

--- utl/jwt.go
+++ utl/jwt.go
@@ -9,7 +9,16 @@ import (
 
 // ServiceName Seperate Char
 const ServiceName = "rbac"
-
+// Struct to parse claim
+type jwtUser struct {
+	ID             int        `json:"id,omitempty"`
+	CustomerID     int        `json:"c,omitempty"`
+	CustomerNumber string     `json:"c_n,omitempty"`
+	Username       string     `json:"u,omitempty"`
+	UserUUID       string     `json:"u_uid,omitempty"`
+	Role           int8 `json:"r,omitempty"`
+	jwt.StandardClaims
+}
 // ParseToken parses token from Authorization header
 func ParseToken(key string, algo string, token string) (*model.AuthUser, error) {
 
@@ -17,7 +26,7 @@ func ParseToken(key string, algo string, token string) (*model.AuthUser, error) 
 	if !(len(parts) == 2 && parts[0] == "Bearer") {
 		return nil, errors.BadRequest(ServiceName, "rbac:utl:ParseToken:invalid")
 	}
-	j,err := jwt.Parse(parts[1], func(token *jwt.Token) (interface{}, error) {
+	j,err := jwt.ParseWithClaims(parts[1], &jwtUser{},func(token *jwt.Token) (interface{}, error) {
 		if jwt.GetSigningMethod(algo) != token.Method {
 			return nil, errors.Forbidden(ServiceName, "rbac:utl:ParseToken:forbidden")
 		}
@@ -26,14 +35,16 @@ func ParseToken(key string, algo string, token string) (*model.AuthUser, error) 
 	if err != nil || !j.Valid{
 		return nil,err
 	}
-	claims := j.Claims.(jwt.MapClaims)
+	if claims, ok := j.Claims.(*jwtUser);ok {
+		return &model.AuthUser{
+			ID:             claims.ID,
+			Username:       claims.Username,
+			CustomerNumber: claims.CustomerNumber,
+			CustomerID:     claims.CustomerID,
+			UserUuid:       claims.UserUUID,
+			Role:           model.AccessRole(claims.Role),
+		}, nil
+	}
 	//check cache
-	return &model.AuthUser{
-		ID:             claims["id"].(int),
-		Username:       claims["u"].(string),
-		CustomerNumber: claims["c_n"].(string),
-		CustomerID:     claims["c"].(int),
-		UserUuid:       claims["u_uid"].(string),
-		Role:           model.AccessRole(int8(claims["r"].(float64))),
-	}, nil
+	return nil, nil
 }
