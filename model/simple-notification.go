b6c6fe6a812f809079ab00ffd9b4b6dc623f1a4d
Phạm Nguyên Cát <dtpham258@gmail.com>
2020-02-03 17:38:12 +0700

    update simple notification model

--- model/simple-notification.go
+++ model/simple-notification.go
@@ -5,7 +5,6 @@ Notification logs
 - use security type  (safety,security) and gateway mac address to identity job id.
 */
 type Notification struct {
-	tableName struct{} `pg:",discard_unknown_columns"`
 	Base
 	ThingName          string             `json:"thingName,omitempty"`
 	ThingDisplayName   string             `json:"thingDisplayName,omitempty"`
@@ -16,15 +15,15 @@ type Notification struct {
 	ZoneName           string             `json:"zoneName,omitempty"`
 	ZoneDisplayName    string             `json:"zoneDisplayName,omitempty"`
 	Template           string             `json:"template,omitempty"`
-	Status             NotificationStatus `sql:", default:0" json:"status,omitempty"`
-	Type               NotificationType   `sql:", default:1" json:"type,omitempty"`
+	Status             NotificationStatus `pg:", default:0" json:"status,omitempty"`
+	Type               NotificationType   `pg:", default:1" json:"type,omitempty"`
 	CustomerNumber     string             `json:"customerNumber,omitempty"`
 	MessageKey         string             `json:"message_key,omitempty"`
 	State              int                `json:"state,omitempty"`
 	DeviceID           int8               `json:"deviceId,omitempty"`
 	Timezone           string             `json:"timezone,omitempty"`
 
-	Localizes []Localize `sql:"-" pg:"fk:notification_id" json:"localizes,omitempty"`
+	Localizes []Localize `pg:"-"  json:"localizes,omitempty"`
 }
 
 // Localize logs
