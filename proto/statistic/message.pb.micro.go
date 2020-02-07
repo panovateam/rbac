f784008ff821acd0cf8a9878e46886bb97c459f0
Phạm Nguyên Cát <dtpham258@gmail.com>
2020-02-07 15:30:44 +0700

    [proto][statistic] add get map direction api

--- proto/statistic/message.pb.micro.go
+++ proto/statistic/message.pb.micro.go
@@ -37,6 +37,7 @@ var _ server.Option
 type SystemSvcService interface {
 	GetCallingActivating(ctx context.Context, in *proto1.Request, opts ...client.CallOption) (*proto1.Response, error)
 	GetCountAPI(ctx context.Context, in *proto1.Request, opts ...client.CallOption) (*proto1.Response, error)
+	GetMapDirectionAPI(ctx context.Context, in *proto1.Request, opts ...client.CallOption) (*proto1.Response, error)
 }
 
 type systemSvcService struct {
@@ -77,17 +78,29 @@ func (c *systemSvcService) GetCountAPI(ctx context.Context, in *proto1.Request, 
 	return out, nil
 }
 
+func (c *systemSvcService) GetMapDirectionAPI(ctx context.Context, in *proto1.Request, opts ...client.CallOption) (*proto1.Response, error) {
+	req := c.c.NewRequest(c.name, "SystemSvc.GetMapDirectionAPI", in)
+	out := new(proto1.Response)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // Server API for SystemSvc service
 
 type SystemSvcHandler interface {
 	GetCallingActivating(context.Context, *proto1.Request, *proto1.Response) error
 	GetCountAPI(context.Context, *proto1.Request, *proto1.Response) error
+	GetMapDirectionAPI(context.Context, *proto1.Request, *proto1.Response) error
 }
 
 func RegisterSystemSvcHandler(s server.Server, hdlr SystemSvcHandler, opts ...server.HandlerOption) error {
 	type systemSvc interface {
 		GetCallingActivating(ctx context.Context, in *proto1.Request, out *proto1.Response) error
 		GetCountAPI(ctx context.Context, in *proto1.Request, out *proto1.Response) error
+		GetMapDirectionAPI(ctx context.Context, in *proto1.Request, out *proto1.Response) error
 	}
 	type SystemSvc struct {
 		systemSvc
@@ -106,4 +119,8 @@ func (h *systemSvcHandler) GetCallingActivating(ctx context.Context, in *proto1.
 
 func (h *systemSvcHandler) GetCountAPI(ctx context.Context, in *proto1.Request, out *proto1.Response) error {
 	return h.SystemSvcHandler.GetCountAPI(ctx, in, out)
+}
+
+func (h *systemSvcHandler) GetMapDirectionAPI(ctx context.Context, in *proto1.Request, out *proto1.Response) error {
+	return h.SystemSvcHandler.GetMapDirectionAPI(ctx, in, out)
 }
